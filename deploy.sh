eb86e594f76f82e7a02a910732b37aef1988ba02
Michael Huang <mhuang74@gmail.com>
2022-09-05 02:05:35 +0800

    script to deploy to aws

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -x
+rm test-wine79.tar.gz
+tar -z -cf test-wine79.tar.gz public/*
+scp -i ~/.ssh/aws-wordpress.pem test-wine79.tar.gz bitnami@35.83.167.212:~/
+ssh -i ~/.ssh/aws-wordpress.pem bitnami@35.83.167.212 "mv -f htdocs/test-wine79 htdocs/test-wine79.1; tar -zxf test-wine79.tar.gz; mv -f public htdocs/test-wine79"
